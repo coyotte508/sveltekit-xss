d56b018f5bbfe41f4c6d4eb4f5a363b2c2de2d07
coyotte508 <coyotte508@gmail.com>
2023-06-20 11:39:05 +0200

    XSS v2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="js">
+  let text = `test'"></textarea><script` + `>alert('BIM');</sc` + `ript>`;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<textarea value={`test'"></textarea><script>alert('BIM');</script>`} />
+<textarea bind:value={text} />
